734076e7dab6c6af15b2f018d3f1a579ac94b512
Christopher White <Maverick@Christophers-MacBook-Air.local>
2017-11-28 09:15:32 -0700

    added production connection

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/motogx'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 }

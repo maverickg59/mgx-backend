a1f6a678cc54b1d16c7dcca0505d07d5b1d7667e
Christopher White <christopher.evan.white88@gmail.com>
2017-12-07 11:51:58 -0700

    uploads to S3 trabajando sin validación

--- api/items.js
+++ api/items.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const queries = require('../db/queries')
 const skipperS3 = require('skipper-s3')
-const fs = require('fs');
 
 function isValidId(req, res, next) {
   if (!isNaN(req.params.id)) return next()
@@ -10,6 +9,7 @@ function isValidId(req, res, next) {
 }
 
 function validItem(item) {
+  // const item = req.body
   const hasCat = typeof item.category_id == 'number'
   const hasBrand = typeof item.brand == 'string' && item.brand.trim() != ''
   const hasModel = typeof item.model == 'string' && item.model.trim() != ''
@@ -19,17 +19,13 @@ function validItem(item) {
   const hasCond = !isNaN(item.condition_id)
   const hasPhoto = typeof item.photo_url == 'string' && item.photo_url.trim() != ''
   const hasBool = typeof item.featured == 'boolean'
-  return (
-    hasCat &&
-    hasBrand &&
-    hasModel &&
-    hasCity &&
-    hasSize &&
-    hasState &&
-    hasCond &&
-    hasPhoto &&
-    hasBool
-  )
+  console.log(item)
+  if ( hasCat && hasBrand && hasModel && hasCity && hasSize && hasState && hasCond && hasPhoto && hasBool)
+  {
+    return true
+  } else {
+    throw new Error('Invalid Item')
+  }
 }
 
 router.get('/', (req, res) => {
@@ -48,28 +44,40 @@ router.get('/:id', isValidId, (req, res, next) => {
   })
 })
 
-router.post('/', (req, res, next) => {
-  if (validItem(req.body)) {
-    queries.create(req.body).then(items => {
-      res.json(items[0])
-    })
-  } else {
-    next(new Error('Invalid item!'))
-  }
-})
+// router.post('/', (req, res, next) => {
+//   if (validItem(req.body)) {
+    // queries.create(req.body).then(items => {
+    //   res.json(items[0])
+    // })
+//   } else {
+//     next(new Error('Invalid item!'))
+//   }
+// })
 
-router.put('/upload', (req, res, next) => {
+router.post('/', (req, res, next) => {
   req.file('file')
     .upload({
-      // Required
       adapter: skipperS3,
       key: 'AKIAILRSW7JDKJAWIAXQ',
       secret: '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',
       bucket: 'mgx-photos',
     }, function whenDone(err, uploadedFiles) {
-      if (err) return next(err);
+      console.log('data payload:', req.body)
+      // if (validItem(req.body)) {
+      //   console.log('valid upload:', req.body)
+      // }
+      if (err) return next(err)
       console.log('uploadedFiles', uploadedFiles)
-      return res.status(200).send(uploadedFiles);
+      const url = uploadedFiles.length > 0 ? uploadedFiles[0].extra.Location : ''
+      const updData = req.body
+      delete updData.file
+      updData.photo_url = url
+      console.log('Queries.Update:', updData)
+      queries.create(updData)
+        .then(items => {
+          res.json(items[0])
+        })
+      // return res.status(200).send(url)
     })
 })
 

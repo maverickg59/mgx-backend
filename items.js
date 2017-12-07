7de4cea659d769aff5cd9b8ebb9dcb89a6398bf6
Christopher White <christopher.evan.white88@gmail.com>
2017-12-07 15:06:41 -0700

    updated dummy data

--- items.js
+++ items.js
@@ -3,12 +3,12 @@ module.exports = [
     category_id: 1,
     price: '$80.00',
     brand: 'Alpinestars',
-    model: 'SMX-5',
-    city: 'Denver',
-    size_id: 1,
-    state_id: 1,
-    condition_id: 1,
-    photo_url: 'www.google.com',
+    model: 'GP Plus R',
+    city: 'Tempe',
+    size_id: 6,
+    state_id: 2,
+    condition_id: 8,
+    photo_url: 'https://www.alpinestars.com/media/catalog/product/cache/small_image/400x400/beff4985b56e3afdbeabfc89641a4582/3/5/3556517_1240_gp-plus-r_glove_web.jpg',
     featured: true,
     gender: '0'
   }
